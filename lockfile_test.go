6779090896afa34b749dc846b251ec0c2874dc59
Simon Buckle <simon.buckle@gmail.com>
2020-02-07 14:43:23 +0000

    Added initial tests

---
+++ lockfile_test.go
@@ -0,0 +1,39 @@
+package lockfile
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestLockFileExists(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "lock")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	lock := New(tmpfile.Name())
+	if err := lock.Lock(); err == nil {
+		t.Error("Call to lock file should have failed")
+	}
+}
+
+func TestLockUnlock(t *testing.T) {
+	//tmpfile, err := ioutil.TempFile("", "lock")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer os.Remove(tmpfile.Name())
+	lock := New("/tmp/test.lock")
+
+	err := lock.Lock()
+	if err != nil {
+		t.Error("Failed to acquire file lock")
+		return
+	}
+	err = lock.Unlock()
+	if err != nil {
+		t.Error("Failed to unlock file")
+	}
+}
